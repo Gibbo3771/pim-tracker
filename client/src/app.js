6cc9dd4ee60c78a408a34afaab707bad983dc294
Kanokwan Sritawan <k.sritawan@gmail.com>
2019-04-19 14:48:52 +0100

    got model working

--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,12 @@
 const Crime = require("./models/crime.js");
+const RequestHelper = require("./helpers/request_helper.js");
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("JS loaded");
 
-  const crime = new Crime("https://data.police.uk/api/crimes-at-location");
+  // const crime = new Crime("https://data.police.uk/api/crimes-at-location");
+  const rh = new RequestHelper("https://data.police.uk/api/crimes-at-location");
+  rh.getCrimesAtLocation("2017-02", "52.629729", "-1.131592", 1).then(res => {
+    console.log(res);
+  });
 });
