808772e9a35d5f936e78534a130a87150177e36c
Kanokwan Sritawan <k.sritawan@gmail.com>
2019-04-19 15:47:24 +0100

    changed this.month to this.date

--- client/src/models/crime.js
+++ client/src/models/crime.js
@@ -3,7 +3,7 @@ const PubSub = require("../helpers/pub_sub.js");
 const Crime = function(params) {
   this.category = params.category;
   this.streetName = params.location.street.name;
-  this.month = params.month;
+  this.date = params.month;
   this.lat = params.location.latitude;
   this.lng = params.location.longitude;
 };
