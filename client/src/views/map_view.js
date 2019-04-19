1f94c912d6d5fe01aeb1a6f6bdf8bcce341c402a
Kanokwan Sritawan <k.sritawan@gmail.com>
2019-04-19 15:33:32 +0100

    created new function - createTileLayer

--- client/src/views/map_view.js
+++ client/src/views/map_view.js
@@ -1,10 +1,17 @@
 require("leaflet");
 
-const MapView = function() {};
+const MapView = function() {
+  this.map = null;
+  this.accessToken =
+    "pk.eyJ1Ijoic2llcnJhdGFuZ28zNCIsImEiOiJjanVvNW96bDcwbXN4NDRwcDhhcGV3YWx3In0.Sz0BPdrbZfoIjICX83oGhA";
+};
 
 MapView.prototype.render = function() {
-  const mymap = L.map("map").setView([51.505, -0.09], 13);
+  this.map = L.map("map").setView([51.505, -0.09], 13);
+  this.createTileLayer();
+};
 
+MapView.prototype.createTileLayer = function() {
   L.tileLayer(
     "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
     {
@@ -12,10 +19,8 @@ MapView.prototype.render = function() {
         'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
       id: "mapbox.streets",
-      accessToken:
-        "pk.eyJ1Ijoic2llcnJhdGFuZ28zNCIsImEiOiJjanVvNW96bDcwbXN4NDRwcDhhcGV3YWx3In0.Sz0BPdrbZfoIjICX83oGhA"
+      accessToken: this.accessToken
     }
-  ).addTo(mymap);
+  ).addTo(this.map);
 };
-
 module.exports = MapView;
