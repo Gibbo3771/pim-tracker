77f6660ac62f8bcf367afa6a27a681b74a5ed588
Mateusz <matstanczak@gmail.com>
2019-04-19 16:35:03 +0100

    added render function

--- client/src/views/crime_list_view.js
+++ client/src/views/crime_list_view.js
@@ -1,5 +1,17 @@
 const PubSub = require("../helpers/pub_sub.js");
+const CrimeItemView = require("./crime_item_view.js");
 
-const CrimeListView = function(container) {
-  this.container = container;
+const CrimeListView = function(crimeListElement) {
+  this.element = crimeListElement;
 };
+
+CrimeListView.prototype.render = function(items) {
+  this.element.innerHTML = "";
+  const div = document.createElement("div");
+  const crimeListView = new CrimeItemView(this.container);
+  items.forEach(item => {
+    crimeListView.render(item);
+  });
+};
+
+module.exports = CrimeListView;
