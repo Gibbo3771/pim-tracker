a43b71edd4c11d2cd586a66ddb96f37402fc7d72
Stephen Gibson <gibson.stephen92@gmail.com>
2019-04-21 14:07:56 +0100

    implemented basic modal

--- client/src/views/modal/modal_view.js
+++ client/src/views/modal/modal_view.js
@@ -1,4 +1,5 @@
 const Modal = function(width, height, opacity, content = null) {
+  this.container = null;
   this.width = width;
   this.height = height;
   this.opacity = opacity;
@@ -6,8 +7,9 @@ const Modal = function(width, height, opacity, content = null) {
 };
 
 Modal.prototype.render = function() {
-  if (this.content === null) return;
+  if (this.content === null) return; // Never render if content is not set
   document.body.appendChild(this.createBackDrop());
+  document.body.appendChild(this.create());
 };
 
 Modal.prototype.createBackDrop = function() {
@@ -17,7 +19,22 @@ Modal.prototype.createBackDrop = function() {
   element.style.height = "100vh";
   element.style.background = "black";
   element.style.opacity = this.opacity;
+  element.addEventListener("click", this.close);
   return element;
+};
+
+Modal.prototype.create = function() {
+  this.container = document.createElement("div");
+  this.container.id = "modal";
+  this.container.style.width = this.width;
+  this.container.style.height = this.height;
+  this.container.appendChild(this.content);
+  return this.container;
+};
+
+Modal.prototype.close = function(evt) {
+  evt.preventDefault();
+  document.getElementById("modal-backdrop").remove();
 };
 
 module.exports = Modal;
