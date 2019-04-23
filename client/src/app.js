319b61bdb707e42af09458c1dc1101be2146c03f
user <gibson.stephen92@gmail.com>
2019-04-23 11:21:47 +0100

    refactored the mapview and request helper

--- client/src/app.js
+++ client/src/app.js
@@ -9,17 +9,7 @@ const getData = evt => {
     "https://data.police.uk/api/crimes-street/all-crime"
   );
 
-  const { latlng1, latlng2, latlng3, latlng4 } = evt.detail;
-  rq.getCrimeInRectangle(
-    latlng1.lat,
-    latlng1.lng,
-    latlng2.lat,
-    latlng2.lng,
-    latlng3.lat,
-    latlng3.lng,
-    latlng4.lat,
-    latlng4.lng
-  )
+  rq.getCrimeInRectangle(evt.detail)
     .then(res => {
       PubSub.publish("App:top-10-crime", res.splice(0, 10));
       PubSub.publish("App:number-of-crime", res.length);
--- client/src/helpers/beautiful_latlng_shapes.js
+++ client/src/helpers/beautiful_latlng_shapes.js
@@ -1,17 +1,17 @@
 const Rectangle = function(rect, roundTo = 3) {
-  latlng1 = {
+  this.latlng1 = {
     lat: rect._latlngs[0][0].lat.toFixed(roundTo),
     lng: rect._latlngs[0][0].lng.toFixed(roundTo)
   };
-  latlng2 = {
+  this.latlng2 = {
     lat: rect._latlngs[0][1].lat.toFixed(roundTo),
     lng: rect._latlngs[0][1].lng.toFixed(roundTo)
   };
-  latlng3 = {
+  this.latlng3 = {
     lat: rect._latlngs[0][2].lat.toFixed(roundTo),
     lng: rect._latlngs[0][2].lng.toFixed(roundTo)
   };
-  latlng4 = {
+  this.latlng4 = {
     lat: rect._latlngs[0][3].lat.toFixed(roundTo),
     lng: rect._latlngs[0][3].lng.toFixed(roundTo)
   };
