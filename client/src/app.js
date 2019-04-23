96ab0d996cda340e99d9bd1dc534c85af64673a2
user <gibson.stephen92@gmail.com>
2019-04-23 11:39:38 +0100

    refactored app, added app container

--- client/src/app.js
+++ client/src/app.js
@@ -1,27 +1,13 @@
+const AppContainer = require("./app_container");
 const MapView = require("./views/map_view/map_view.js");
 const CrimeListView = require("./views/crime_list_view.js");
 const PubSub = require("./helpers/pub_sub.js");
 const RequestHelper = require("./helpers/request_helper.js");
 require("leaflet");
 
-const getData = evt => {
-  const rq = new RequestHelper(
-    "https://data.police.uk/api/crimes-street/all-crime"
-  );
-
-  rq.getCrimeInRectangle(evt.detail)
-    .then(res => {
-      PubSub.publish("App:top-10-crime", res.splice(0, 10));
-      PubSub.publish("App:number-of-crime", res.length);
-    })
-    .catch(res => {
-      PubSub.publish("App:data-overload");
-    });
-};
-
 document.addEventListener("DOMContentLoaded", () => {
-  PubSub.subscribe("MapView:area-modified", getData);
-
+  const appContainer = new AppContainer();
+  appContainer.bindEvents();
   const map = new MapView();
   map.render();
   map.bindEvents();
