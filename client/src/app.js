eb4e9c6f5bd9717b34f320ae41a443636f02606b
user <gibson.stephen92@gmail.com>
2019-04-24 11:19:51 +0100

    refactored html and css to accomodate new button/dropdown

--- client/src/app.js
+++ client/src/app.js
@@ -7,6 +7,7 @@ const RequestHelper = require("./helpers/request_helper.js");
 const CrimeDetailView = require("./views/crime_detail_view");
 const AboutView = require("./views/about_view.js");
 const Chart = require("./views/high_chart.js");
+const ButtonAbout = require("./components/button_about/button_about");
 require("leaflet");
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -17,14 +18,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const map = new MapView();
   map.render();
   map.bindEvents();
-
   const listView = new CrimeListView();
   listView.bindEvents();
-
   const aboutView = new AboutView();
-  document.getElementById("about").addEventListener("click", evt => {
-    appContainer.handleAboutButtonClick(evt);
-  });
   const chart = new Chart();
   chart.bindEvents();
+
+  const buttonAbout = new ButtonAbout();
+  const dropdownDate = new DropdownDate();
 });
